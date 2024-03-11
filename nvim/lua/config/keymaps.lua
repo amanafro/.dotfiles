1eefbeaa45203c8e5a1f8fcd7840fca9844d1a83
amanafro <amanauelisaak15@gmail.com>
2024-03-11 16:47:47 +0100

    commiting my neovim and tmux config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- overwrite lazyvim mappings with vim-tmux-navigator mappings
+-- see: https://github.com/christoomey/vim-tmux-navigator/blob/master/plugin/tmux_navigator.vim
+-- https://github.com/LazyVim/LazyVim/discussions/322#discussioncomment-5548050
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<M-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<M-Right>", "<cmd>vertical resize +1<cr>", { desc = "Increase window width" })
